2d1d8a6b8eff918b75fed119e76b2132b4fd9ae9
pixelhowl <p1x3lh0wl@gmail.com>
2022-12-04 12:17:46 +0900

    Apply yapf

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,15 +3,15 @@ import os
 
 HOME = os.path.abspath("./")
 KOTLIN_HOME = os.path.abspath("../KotlinInside/build/libs")
-GALLARY_NAME = "kizunaai" #갤러리 id
-CUR_YEAR = START_YEAR=datetime.datetime.now().year
-END_YEAR=datetime.datetime.now().year
-CUR_MONTH =datetime.datetime.now().month-1 # 갤러리, 연, 월
+GALLARY_NAME = "kizunaai"  #갤러리 id
+CUR_YEAR = START_YEAR = datetime.datetime.now().year
+END_YEAR = datetime.datetime.now().year
+CUR_MONTH = datetime.datetime.now().month - 1  # 갤러리, 연, 월
 POST_FILENAME = "post.json"
 COMMENT_FILENAME = "comment.json"
 WORD_DIR = "word"
-WORD_DAY_FILENAME = "_day.json" 
-WORD_NIGHT_FILENAME = "_night.json" 
+WORD_DAY_FILENAME = "_day.json"
+WORD_NIGHT_FILENAME = "_night.json"
 DCINSIDE_URL = "http://m.dcinside.com/api"
 
-from . import kotilnside, parsers, database, vtuber_dict, word_dict
+from . import kotilnside, parsers, database, vtuber_dict, word_dict, youtube, pptx_utils
